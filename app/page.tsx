df9edae10884545d0e0fe906103330b4e35d4553
Flamingo <Flamingo7562@gmail.com>
2025-01-24 22:07:34 +0900

    First Commit - Create Repository

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function Tomato() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Hello! NextJS!</h1>
+    </div>
+  );
+}
