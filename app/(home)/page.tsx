a5774cf7d95571a4e1f28cd1abad72220d1cc62a
Flamingo <Flamingo7562@gmail.com>
2025-01-27 23:08:19 +0900

    Server Side is interesting

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,21 @@
-"use client";
-import { useState, useEffect } from "react";
+export const metadata = {
+  title: "Home",
+};
 
-export default function Tomato() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>{isLoading ? "Loading..." : JSON.stringify(movies)}</h1>
+      <h1>{JSON.stringify(movies)}</h1>
     </div>
   );
 }
