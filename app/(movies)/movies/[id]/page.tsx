4d256ceeb537e5602c8a80562e442ac534b8a12e
Flamingo <Flamingo7562@gmail.com>
2025-01-28 23:22:20 +0900

    Nice Parallel Fetching

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,28 @@
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
 export default async function MovieDetail({ params }) {
   const { id } = await params;
-  console.log({ params });
-  return <h1>Movies! {id}</h1>;
+  console.log("Start Fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("End Fetching");
+  return (
+    <h1>
+      {movie.title} : {id}
+    </h1>
+  );
 }
