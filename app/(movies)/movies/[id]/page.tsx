2624b7211f3090d960ab09f9a393e501fe622a13
Flamingo <Flamingo7562@gmail.com>
2025-02-03 22:31:15 +0900

    Nice Videos, and cleared the Error. It was stupid URL ever.

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,13 +2,21 @@ import { Suspense } from "react";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({ params }) {
-  const { id } = await params;
+interface IParams {
+  params: { id: string };
+}
+
+export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading Movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
+      <Suspense>
+        <MovieVideos id={id} />
+      </Suspense>
     </div>
   );
 }
+
+export const runtime = "edge";
